5f1b005993db7c0fa88626994639b7df68748c6a
zhanghuaming <szjacky2000@126.com>
2019-03-04 13:45:05 +0800

    optimize add banks

--- app/Http/Controllers/PersonalcenterController.php
+++ app/Http/Controllers/PersonalcenterController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Http\Request;
 use Auth;
+use App\Models\Bank;
 
 class PersonalcenterController extends Controller
 {
@@ -19,16 +20,38 @@ class PersonalcenterController extends Controller
         return view('personal-center.index');
     }
 
+    /** ajax add bank information
+    ** 0  数据正常被添加
+    ** 1  添加数据发生错误
+    ** 2  银行号已被重复使用
+    **  
+    **/
+
     public function bank_add(Request $request)
     {
-    	$data = $request->all();
+    	if($request->isMethod('post')){
 
-        $data['user_id']= Auth::id();
-        unset($data['_token']);
-        unset($data['code']);
+    		$data = $request->all();
 
-        $st = DB::table('banks')->insert($data); 
-    	return ($st == 1) ? -1 : 1;
+    		if(!isset($data['num']) && empty($data['num'])) return 0;
+
+    		$bank = new Bank;
+    		$res = $bank->is_exists($data['num']);
+    		if($res == 1) return 2;
+
+	        $data['user_id']= Auth::id();
+	        unset($data['_token']);
+	        unset($data['code']);
+	        $st = DB::table('banks')->insert($data); 
+	    	return ($st == 1) ? -1 : 1;
+
+    	}else{
+
+    		return 0;
+
+    	}
+
+
     }
 
 
--- public/js/bank.js
+++ public/js/bank.js
@@ -1,9 +1,26 @@
 //基本类
+
+var layer;
+
+layui.use('layer', function(){
+    layer = layui.layer;
+});
+
+function msg(title, content, time = 5000, icon = 1){
+    layer.open({
+        time     : time
+        ,title   : title
+        ,content : content
+        ,icon    : icon
+    });
+}
+
 var Account = function( form_id ){
     this.form_id = form_id;
     this.is_success = true;
     this.ms = [];
 };
+
 Account.prototype = {
     //根据ID取得表单数据key=val串
     get_form_data:function(){
@@ -16,16 +33,18 @@ Account.prototype = {
         var url = url;
         var dat = this.ms;
         $.ajax({
-            headers : {'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')},
-            url: url,
-            data: dat,
-            dataType: 'json',
-            type: 'post',
+            // headers  : {'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')},
+            url      : url,
+            data     : dat,
+            dataType : 'json',
+            type     : 'post',
             success: function( data ) {
-                console.log(data);
+                if(data == 1)      msg('系统提示您：', '添加数据发生错误!'    , 5000, 2);
+                else if(data == 2) msg('系统提示您：', '银行卡号已被重复使用!' , 5000, 5);
+                else               msg('系统提示您：', '银行卡号被添加'       , 1000, 1);
             },
             error: function( data ){
-                console.log( data );
+                if(data) msg('Error:', data, 5000, 2); 
             }
         });
     }
